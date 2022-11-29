432e0e974bcb6e5a7200c1d24807fb3016c02292
aresous <aresous@gmail.com>
2022-11-28 19:38:25 -0500

    from solutions page

--- sliding_windows/minSubArrayLen.js
+++ sliding_windows/minSubArrayLen.js
@@ -1,23 +1,38 @@
-function minSubArrayLen(arr, n) {
-  let max = arr[0];
-  let curr = arr[0];
-  let longest = 0;
-  let maxLongest = 0;
-  for (let i = 1; i < arr.length; i++) {
-    // check to see if our current window is greater than or equal to N,
-    //curr+arr[i] = current window
-    console.log(`arr[i]: ${arr[i]}  | curr:  ${curr}`);
-    longest += 1;
-    // if our current window is greater than the expected number, reset our length after applying our max length.
-    console.log(longest, "longest");
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
 
-    if (curr + arr[i] > n) {
-      console.log("trig?");
-      maxLongest = longest;
-      longest = 0;
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then
+    // move the window to right
+    if (total < sum && end < nums.length) {
+      total += nums[end];
+      end++;
     }
-    curr = curr + arr[i];
+    // if current window adds up to at least the sum given then
+    // we can shrink the window
+    else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+    else {
+      break;
+    }
   }
-  return longest;
+
+  return minLen === Infinity ? 0 : minLen;
 }
-console.log(minSubArrayLen([1, 2, 3, 4, 0, 90]), 89);
+
+console.log(
+  minSubArrayLen([2, 3, 1, 2, 4, 3], 7), // 2 -> because [4,3] is the smallest subarray
+  minSubArrayLen([2, 1, 6, 5, 4], 9), // 2 -> because [5,4] is the smallest subarray
+  minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52), // 1 -> because [62] is greater than 52
+  minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39), // 3
+  minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55), // 5
+  minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11), // 2
+  minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) // 0
+);
